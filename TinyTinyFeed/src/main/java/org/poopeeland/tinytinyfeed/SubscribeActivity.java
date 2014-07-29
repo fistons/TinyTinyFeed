4261c73d44eac1189af4529638f20b6f6080a7d4
Eric <mercier.eric@gmail.com>
2014-07-29 13:29:38 +0200

    Create a new activity, used to allow subscription with the widget

---
+++ TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/SubscribeActivity.java
@@ -0,0 +1,17 @@
+package org.poopeeland.tinytinyfeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SubscribeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscribe);
+    }
+
+
+
+}
