dfe89a223c4cc4958d87bb5ca1e24802a208f699
Eric <mercier.eric@gmail.com>
2014-08-03 22:45:14 +0200

    Working on the subscribe activity

--- TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/SubscribeActivity.java
+++ TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/SubscribeActivity.java
@@ -1,17 +1,81 @@
 package org.poopeeland.tinytinyfeed;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.poopeeland.tinytinyfeed.exceptions.CheckException;
+import org.poopeeland.tinytinyfeed.exceptions.NoInternetException;
+import org.poopeeland.tinytinyfeed.exceptions.RequiredInfoNotRegistred;
+import org.poopeeland.tinytinyfeed.widget.WidgetService;
+
+import java.util.concurrent.ExecutionException;
 
 
 public class SubscribeActivity extends Activity {
 
+    private final String TAG = getClass().getSimpleName();
+    private String url;
+    private WidgetService service;
+    private boolean bound;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder binder) {
+            WidgetService.LocalBinder mbinder = (WidgetService.LocalBinder) binder;
+            service = mbinder.getService();
+            bound = true;
+            Log.d(TAG, "bounded!");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            bound = false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_LEFT_ICON);
         setContentView(R.layout.activity_subscribe);
+        getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.ic_launcher);
+
+        /* Bind to the service */
+        Intent intentBound = new Intent(this, WidgetService.class);
+        intentBound.putExtra(WidgetService.ACTIVITY_FLAG, true);
+        bindService(intentBound, mConnection, Context.BIND_AUTO_CREATE);
+
+
+        Intent intent = getIntent();
+        this.url = getIntent().getDataString();
+
+        if (this.url == null) {
+            this.url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        }
+
+        ((EditText) findViewById(R.id.subActUrl)).setText(url);
+
+
     }
 
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bound) {
+            Log.d(TAG, "unbound!");
+            unbindService(mConnection);
+            bound = false;
+        }
+    }
 }
