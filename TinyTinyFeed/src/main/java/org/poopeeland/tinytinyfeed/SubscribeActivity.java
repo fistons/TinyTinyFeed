dd725aa152ae0ada59bb33fdfeec9959b5e009a1
Eric <mercier.eric@gmail.com>
2014-08-04 23:24:24 +0200

    - Load the categories on start up - comments

--- TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/SubscribeActivity.java
+++ TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/SubscribeActivity.java
@@ -1,7 +1,6 @@
 package org.poopeeland.tinytinyfeed;
 
 import android.app.Activity;
-import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -9,9 +8,12 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 import org.json.JSONException;
 import org.poopeeland.tinytinyfeed.exceptions.CheckException;
@@ -19,9 +21,14 @@ import org.poopeeland.tinytinyfeed.exceptions.NoInternetException;
 import org.poopeeland.tinytinyfeed.exceptions.RequiredInfoNotRegistred;
 import org.poopeeland.tinytinyfeed.widget.WidgetService;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-
+/**
+ * Subscribe Activity
+ * Allow to subscribe to a RSS feed from the app
+ * Created by eric on 01/08/14.
+ */
 public class SubscribeActivity extends Activity {
 
     private final String TAG = getClass().getSimpleName();
@@ -35,6 +42,24 @@ public class SubscribeActivity extends Activity {
             service = mbinder.getService();
             bound = true;
             Log.d(TAG, "bounded!");
+            try {
+                List<Category> categories = service.loadCategories();
+                ArrayAdapter<Category> dataAdapter = new ArrayAdapter<Category>(SubscribeActivity.this, android.R.layout.simple_spinner_item, categories);
+                Spinner spinner = (Spinner) findViewById(R.id.catList);
+                spinner.setAdapter(dataAdapter);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (CheckException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (RequiredInfoNotRegistred requiredInfoNotRegistred) {
+                requiredInfoNotRegistred.printStackTrace();
+            } catch (NoInternetException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -47,6 +72,7 @@ public class SubscribeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /* Load the layout and set the image */
         requestWindowFeature(Window.FEATURE_LEFT_ICON);
         setContentView(R.layout.activity_subscribe);
         getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.ic_launcher);
@@ -56,15 +82,22 @@ public class SubscribeActivity extends Activity {
         intentBound.putExtra(WidgetService.ACTIVITY_FLAG, true);
         bindService(intentBound, mConnection, Context.BIND_AUTO_CREATE);
 
-
+        /* Retrieve data from the intent */
         Intent intent = getIntent();
         this.url = getIntent().getDataString();
-
         if (this.url == null) {
             this.url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
         }
 
         ((EditText) findViewById(R.id.subActUrl)).setText(url);
+
+
+        Button b = (Button) findViewById(R.id.subActButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
 
     }
