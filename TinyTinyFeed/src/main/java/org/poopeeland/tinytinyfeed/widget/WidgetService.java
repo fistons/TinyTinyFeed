8b59368d08433d3128218e4a10048ae389e1b1bc
Eric <mercier.eric@gmail.com>
2014-12-13 21:00:38 +0100

    Now can configure the exceprt lenght

--- TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/widget/WidgetService.java
+++ TinyTinyFeed/src/main/java/org/poopeeland/tinytinyfeed/widget/WidgetService.java
@@ -63,6 +63,7 @@ public class WidgetService extends RemoteViewsService {
     private String password;
     private String user;
     private String numArticles;
+    private String excerptLenght;
     private boolean onlyUnread;
     private HttpClient client;
     private File lastListFile;
@@ -86,6 +87,7 @@ public class WidgetService extends RemoteViewsService {
         this.numArticles = preferences.getString(TinyTinyFeedWidget.NUM_ARTICLE_KEY, "");
         this.onlyUnread = preferences.getBoolean(TinyTinyFeedWidget.ONLY_UNREAD_KEY, false);
         this.session = preferences.getString(TinyTinyFeedWidget.SESSION_KEY, null);
+        this.excerptLenght = preferences.getString(TinyTinyFeedWidget.EXCERPT_LENGHT_KEY, getText(R.string.preference_excerpt_lenght_default_value).toString());
         String httpUser = preferences.getString(TinyTinyFeedWidget.HTTP_USER_KEY, "");
         String httpPassword = preferences.getString(TinyTinyFeedWidget.HTTP_PASSWORD_KEY, "");
         this.client = this.getNewHttpClient(httpUser, httpPassword);
@@ -178,6 +180,7 @@ public class WidgetService extends RemoteViewsService {
         jsonObject.put("feed_id", feedId);
         jsonObject.put("limit", this.numArticles);
         jsonObject.put("show_excerpt", "true");
+        jsonObject.put("excerpt_length", this.excerptLenght);
 
         RequestTask task = new RequestTask(this.client, this.url);
         task.execute(jsonObject);
